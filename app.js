d498e418beaf940c9a2fe27fc9912aaa77f9cea5
ibktommy <atomquartz@gmail.com>
2022-08-03 22:08:47 +0100

    Create a separate JS file that will include mini-functions which are exported and imported tothe APP-JS file. Also click-events are added to income/expense and all BTN so as to display their respective lists when clicked

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const balanceEl = document.querySelector('.balane .value')
 const incomeTotalEl = document.querySelector('.income-total')
 const outcomeTotalEl = document.querySelector('.outcome-total')
-const incomeEl = document.querySelector('.#income-tracker')
-const expenseEl = document.querySelector('.#expense-tracker')
+const incomeEl = document.querySelector('#income-tracker')
+const expenseEl = document.querySelector('#expense-tracker')
 const allEl = document.querySelector('#all')
-const incomeList = document.querySelector('.#income-tracker .list')
-const expenseList = document.querySelector('.#expense-tracker .list')
-const allList = document.querySelector('.#all .list')
+const incomeList = document.querySelector('#income-tracker .list')
+const expenseList = document.querySelector('#expense-tracker .list')
+const allList = document.querySelector('#all .list')
 const lists = document.querySelectorAll('.list')
 
 
@@ -15,18 +15,45 @@ const incomeBtn = document.querySelector('.tab2')
 const expenseBtn = document.querySelector('.tab1')
 const allBtn = document.querySelector('.tab3')
 
+console.log(allList, allBtn)
+
 // INPUT BUTTONS
 const addIncome = document.querySelector('.add-income')
-const incomeTitle = document.querySelector('.#income-title-input')
+const incomeTitle = document.querySelector('#income-title-input')
 const incomeAmount = document.querySelector('#income-amount-input')
 
 
 const addExpense = document.querySelector('.add-expense')
-const expenseTitle = document.querySelector('.#expense-title-input')
+const expenseTitle = document.querySelector('#expense-title-input')
 const expenseAmount = document.querySelector('#expense-amount-input')
 
 // NECESSARY VARIABLES
 let ENTRY_LIST = []
 let [balance, income, outcome] = [0,0,0]
 let [deleteIcon, editIcon] = ['fas fa-trash', 'far fa-edit']
+
+/* IMPORT MINI-FUNCTIONS - DISPLAY_FUNCTIONS */
+import { show, hide, active, inactive } from './miniFunctions.js'
+
+// Income-Btn Event Listener
+incomeBtn.addEventListener('click', function () {
+  show(incomeEl)
+  hide([expenseEl, allList])
+  active(incomeBtn)
+  inactive([expenseBtn, allBtn])
+})
+// Expense-Btn Event Listener
+expenseBtn.addEventListener('click', function () {
+  show(expenseEl)
+  hide([incomeEl, allList])
+  active(expenseBtn)
+  inactive([incomeBtn, allBtn])
+})
+// All-Btn Event Listener
+allBtn.addEventListener('click', function () {
+  show(allList)
+  hide([incomeEl, expenseEl])
+  active(allBtn)
+  inactive([incomeBtn, expenseBtn])
+})
 
---
+++ miniFunctions.js
@@ -0,0 +1,17 @@
+function show(element) {
+  element.classList.remove('hide')
+}
+function hide(elements) {
+  elements.forEach((element) => {
+    element.classList.add('hide')
+  })
+}
+function active(element) {
+  element.classList.add('active')
+}
+function inactive(elements) {
+  elements.forEach((element) => element.classList.remove('active'))
+}
+
+
+export { show, hide, active, inactive }
